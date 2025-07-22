c482544b7a666bbc3381ac3c2a9b82a3f4952953
annabelivey <annabelivey@arizona.edu>
2025-07-21 17:00:27 -0700

    Updates

--- ProjectDraft.R
+++ ProjectDraft.R
@@ -7,15 +7,25 @@ library(dplyr)
 install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("arrow")
+
 #import data
 
 CAES_data <- read.csv('C:/Users/eburn/Desktop/EquityAgProjects/EqAgFinalRepo/data/CAES.csv' , stringsAsFactors = TRUE )
 
 #subset data to include only the variables we need
+
 CAES_data <- CAES[c(1, 3, 5, 25, 26, 33, 34)]
 
-#creating a plot to view our data 
+#creating a plot to view our traffic data 
+
 ggplot(CAES_data, aes(x = Traffic )) +
   geom_histogram(binwidth = 1000, fill = "steelblue", color = "white") +
   labs(title = "Histogram of Traffic Density", x = "Values", y = "Frequency") +
+  theme_minimal()
+
+#creating a plot to view our impaired water data 
+
+ggplot(CAES_data, aes(x = `Imp. Water Bodies` )) +
+  geom_boxplot(fill = "steelblue", color = "white") +
+  labs(title = "Boxplot of Impaired Water Bodies", x = "Values", y = "Frequency") +
   theme_minimal()
